3a958300d0a73504fab1d778e76b9d6f7fd70b23
Peter Schlosser <commerce@sonic.net>
2018-06-09 01:21:51 -0700

    Refactor JS warning

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -11,11 +11,11 @@
 
         if (!settings.date)
         {
-            $.error('Date is not defined.');
+            $.error("Date is not defined.");
         }
         if (!Date.parse(settings.date))
         {
-            $.error('Incorrect date format, try using MM/DD/YYYY HH:MM:SS.');
+            $.error("Incorrect date format, try using MM/DD/YYYY HH:MM:SS.");
         }
 
         // Save container
@@ -56,8 +56,10 @@
                 // stop timer
                 clearInterval(interval);
 
-                if (callback && typeof callback === 'function') callback();
-
+                if (callback && typeof callback === "function")
+				{
+					callback();
+				}
                 return;
             }
 
@@ -74,16 +76,16 @@
                 seconds = Math.floor((difference % _minute) / _second);
 
             // fix dates so that it will show two digets
-            days = (String(days).length >= 2) ? days : '0' + days;
-            hours = (String(hours).length >= 2) ? hours : '0' + hours;
-            minutes = (String(minutes).length >= 2) ? minutes : '0' + minutes;
-            seconds = (String(seconds).length >= 2) ? seconds : '0' + seconds;
+            days = (String(days).length >= 2) ? days : "0" + days;
+            hours = (String(hours).length >= 2) ? hours : "0" + hours;
+            minutes = (String(minutes).length >= 2) ? minutes : "0" + minutes;
+            seconds = (String(seconds).length >= 2) ? seconds : "0" + seconds;
 
             // set to DOM
-            container.find('.days').text(days);
-            container.find('.hours').text(hours);
-            container.find('.minutes').text(minutes);
-            container.find('.seconds').text(seconds);
+            container.find(".days").text(days);
+            container.find(".hours").text(hours);
+            container.find(".minutes").text(minutes);
+            container.find(".seconds").text(seconds);
         };
 
         // start
