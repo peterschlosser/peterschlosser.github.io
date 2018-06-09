5af1e1432f23f7ca75299a58edf272fbb1267877
Peter Schlosser <commerce@sonic.net>
2018-06-09 01:15:29 -0700

    Refactor JS warning

--- js/app.js
+++ js/app.js
@@ -2,8 +2,13 @@ jQuery(function()
 {
     "use strict";
 	var d = new Date();
-	var startdate = (("0"+(d.getMonth()+1)).slice(-2) + "/" + "0"+d.getDate()).slice(-2) + "/" + d.getFullYear() + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2) + ":00";
-    var options = {
+	var startdate = ("0"+(d.getMonth()+1)).slice(-2) + "/"
+		+ ("0"+(d.getDate())).slice(-2) + "/" 
+		+ d.getFullYear() + " " 
+		+ ("0" + d.getHours()).slice(-2) + ":" 
+		+ ("0" + d.getMinutes()).slice(-2) 
+		+ ":00";
+	var options = {
         wrapper: ".wrapper",
         minHeight: 500
     };
